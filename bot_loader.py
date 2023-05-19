34a1a72bae7c440ec80ab816ee7c9ca4a2fbcb62
Zwick Vitaly <dezann-surgut@rambler.ru>
2023-05-19 20:42:03 +0300

    core_refactor_02

--- bot_loader.py
+++ bot_loader.py
@@ -20,73 +20,42 @@ storage = MemoryStorage()
 bot = Bot(TOKEN, parse_mode='HTML')
 dispatcher = Dispatcher(bot, storage=storage)
 
-dispatcher.register_message_handler(
-    help_handler,
-    filters.CommandHelp(),
-    state="*",
-)
-dispatcher.register_message_handler(
-    start_handler,
-    filters.CommandStart(),
-    state="*",
-)
-dispatcher.register_message_handler(
-    stop_handler,
-    filters.Command('stop'),
-    state="*",
-)
-dispatcher.register_message_handler(
-    main_menu_handler,
-    filters.Text('< Главное меню'),
-    state=[SempreFSM.today_shift,
-           SempreFSM.tomorrow_shift,
-           SempreFSM.menu_choice,
-           SempreFSM.day_choice]
-)
+register_handlers = [
+    (help_handler, filters.CommandHelp(), {"state": "*"}),
 
-dispatcher.register_message_handler(
-    moscow_weather_handler,
-    filters.Text('Погода в Москве'),
-    state=SempreFSM.main_menu,
-)
-dispatcher.register_message_handler(
-    curr_loc_weather_handler,
-    content_types=ContentTypes.LOCATION,
-    state=SempreFSM.main_menu,
-)
+    (start_handler, filters.CommandStart(), {"state": "*"}),
 
-dispatcher.register_message_handler(
-    dish_menu_handler,
-    filters.Text('Меню'),
-    state=SempreFSM.main_menu,
-)
-dispatcher.register_message_handler(
-    dish_handler,
-    lambda message: message.text != '< Главное меню',
-    state=SempreFSM.menu_choice,
-)
-dispatcher.register_message_handler(
-    day_handler,
-    filters.Text('Смена'),
-    state=SempreFSM.main_menu,
-)
-dispatcher.register_message_handler(
-    worker_handler,
-    filters.Text(['Сегодня', 'Завтра']),
-    state=SempreFSM.day_choice
-)
-dispatcher.register_message_handler(
-    schedule_workers_handler_today,
-    filters.Text(['Грузчики', 'Хостес', 'Раннеры', 'Клининг', 'Официанты']),
-    state=SempreFSM.today_shift
-)
-dispatcher.register_message_handler(
-    schedule_workers_handler_tomorrow,
-    filters.Text(['Грузчики', 'Хостес', 'Раннеры', 'Клининг', 'Официанты']),
-    state=SempreFSM.tomorrow_shift
-)
-dispatcher.register_message_handler(
-    new_table_handler,
-    filters.Regexp(regexp=compile(r'^/new_table.*', IGNORECASE)),
-    state=SempreFSM.main_menu
-)
+    (stop_handler, filters.Command("stop"), {"state": "*"}),
+
+    (main_menu_handler, filters.Text("< Главное меню"), {"state": [SempreFSM.today_shift,
+                                                                   SempreFSM.tomorrow_shift,
+                                                                   SempreFSM.menu_choice,
+                                                                   SempreFSM.day_choice]}),
+
+    (moscow_weather_handler, filters.Text('Погода в Москве'), {"state": SempreFSM.main_menu},),
+
+    (curr_loc_weather_handler, {"content_types": ContentTypes.LOCATION, "state": SempreFSM.main_menu}),
+
+    (dish_menu_handler, filters.Text("Меню"), {"state": SempreFSM.main_menu}),
+
+    (dish_handler, lambda message: message.text != '< Главное меню', {"state": SempreFSM.menu_choice}),
+
+    (day_handler, filters.Text("Смена"), {"state": SempreFSM.main_menu}),
+
+    (worker_handler, filters.Text(['Сегодня', 'Завтра']), {"state": SempreFSM.day_choice}),
+
+    (schedule_workers_handler_today, filters.Text(['Грузчики', 'Хостес', 'Раннеры', 'Клининг', 'Официанты']),
+     {"state": SempreFSM.today_shift}),
+
+    (schedule_workers_handler_tomorrow, filters.Text(['Грузчики', 'Хостес', 'Раннеры', 'Клининг', 'Официанты']),
+     {"state": SempreFSM.tomorrow_shift}),
+
+    (new_table_handler, filters.Regexp(regexp=compile(r'^/new_table.*', IGNORECASE)),
+     {"state": SempreFSM.main_menu})
+]
+
+for handler in register_handlers:
+    if isinstance(handler[1], dict):
+        dispatcher.register_message_handler(handler[0], **handler[1])
+    else:
+        dispatcher.register_message_handler(handler[0], handler[1], **handler[2])
